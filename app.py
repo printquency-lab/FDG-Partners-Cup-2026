bd736736d57e6f39d9e2659afb882509e1dd7263
pq <printquency@gmail.com>
2026-06-12 14:45:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -126,7 +126,7 @@ st.markdown("---")
 # =========================================================================
 # CONFIGURATION TARGETS
 # =========================================================================
-GAS_URL = "https://script.google.com/macros/s/AKfycbybJ2vEL2cCRtQo6q6zCbQfZBEzAh90cn4mJ07peIFWU3-r1xMquVEgmY7qwYMo1gUuzg/exec" 
+GAS_URL = "https://script.google.com/macros/s/AKfycbyngdqZQ4_UCHPrZG0geGYi2slOgs-v50vJmBR27lTiroyyJPVIjqY1VIO9IGNRjtXgyA/exec" 
 SPREADSHEET_ID = "1PUcUeTApYbCjYbkEn9BzItRjRArLKMVnIqz_7Mtd7-w"
 
 # State Management
